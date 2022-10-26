84e2743365f19dedea48b672e8610ecf6340e486
http.sslbackend <victorpadua.ti@gmail.com>
2022-10-25 21:33:30 -0300

    EXERCÍCIO 1

---
+++ EXERCÍCIO_1.py
@@ -0,0 +1,16 @@
+# Crie um programa que contenha as variáveis a, b, c, d.
+
+a = 10
+b = 10.5
+c = "Victor"
+d = True
+
+print ("O valor da variável A é: " + str(a) + " e o tipo dela é ")
+print(type(a))
+print ("O valor da variável B é: " + str(b) + " e o tipo dela é ")
+print(type(b))
+print ("O valor da variável C é: " + str(c) + " e o tipo dela é ")
+print(type(c))
+print ("O valor da variável D é: " + str(d) + " e o tipo dela é ")
+print(type(d))
+
---
+++ EXERCÍCIO_2.py
@@ -0,0 +1,2 @@
+#Crie um programa para receber valores em variáveis de string e inteiro e mostre os valores ao final.
+
