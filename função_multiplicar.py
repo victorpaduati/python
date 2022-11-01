8ca01db48b42b1dddf3fcfb880bb1322b35951a8
http.sslbackend <victorpadua.ti@gmail.com>
2022-11-01 19:54:59 -0300

    Atividade 01/11/2022

---
+++ função_multiplicar.py
@@ -0,0 +1,13 @@
+#Crie um programa com uma função para multiplicar dois números
+# e o algoritmo mostrar o resultado.
+
+print("Algoritmo para Multiplicação")
+
+nx = float(input("Digite os números a serem multiplicados: "))
+ny = float(input(": "))
+
+def multiplica(n1,n2):
+    resultado=n1*n2
+    return print("O resultado da multiplicação é: ",resultado)
+
+multiplica(nx,ny)
