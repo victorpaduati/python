f003340beae6dbf87e674ff247fb0ba05aec5385
http.sslbackend <victorpadua.ti@gmail.com>
2022-11-03 19:37:23 -0300

    Atividade Corrigida 03/11/2022

--- Atividade 5/consoantes.py
+++ Atividade 5/consoantes.py
@@ -2,20 +2,13 @@
 #e diga quantas consoantes foram lidas.
 #Mostre as consoantes.
 
-print("Programa Contagem de Consoantes!")
 
-vetor = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
-
-for i in range(10):
-    vetor[i] = input("Digite uma letra para cada posição: ")
-print(vetor)
-
-contador=0
+vetor = []
 
 for i in range(10):
-    if not (vetor[i] == str("a") or vetor[i]=="e" or vetor[i]=="i" or vetor[i]=="o" or vetor[i]=="u"):
-        print("consoante na posição " + str(i+1) + ": ", vetor[i])
-        contador=contador+1
+    consoante = input("Digite uma letra de A - Z: ").upper()
+    if consoante not in ["A", "E", "I", "O", "U"]:
+        vetor.append(consoante)
 
-print("o número total de consoantes é", contador)
-
+print("O vetor possui ", len(vetor), " posições de consoantes lidas")
+print("Consoantes digitadas: ", vetor)
